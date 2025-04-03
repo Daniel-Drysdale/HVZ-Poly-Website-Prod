c79ae769601651473e2eb8b3b7e54903d42e4086
Danny Drysdale <126314303+Daniel-Drysdale@users.noreply.github.com>
2025-04-03 16:56:13 -0400

    Update Logo.tsx

--- HVZ_Frontend/src/components/Logo.tsx
+++ HVZ_Frontend/src/components/Logo.tsx
@@ -3,7 +3,18 @@ import Circle_logo from "../assets/Circle_logo.png";
 function Logo() {
   return (
     <>
-      <img src={Circle_logo} className="" style={{}}></img>
+      <img
+        src={Circle_logo}
+        className=""
+        style={{
+          minWidth: ".5%",
+          maxWidth: "75px",
+          position: "absolute",
+          marginLeft: "2px",
+          top: "5px",
+          marginTop: "10px",
+        }}
+      ></img>
     </>
   );
 }
